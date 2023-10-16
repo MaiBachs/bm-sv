95c7accaaa3a18f7a37ec8aa38e1611856edf40c
MaiBachs <maibach699@gmail.com>
2023-10-16 08:08:04 +0700

    cross

--- demo/src/main/java/com/example/demo/controller/CreditCardsController.java
+++ demo/src/main/java/com/example/demo/controller/CreditCardsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/credit-card")
+@CrossOrigin(origins = "*")
 public class CreditCardsController {
     @Autowired
     private CreditCardsService creditCardsService;
